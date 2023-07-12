18ed61c1fcc25a1604d792983336492ccf7184de
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-07-12 20:18:56 +0530

    update sever initEngineer

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 const app = require('./app');
 const { connectDB } = require('./Configs/config.DB');
 const AdminModel = require('./Models/AdminModel');
+const EngineerModel = require('./Models/EngineerModel');
 
 // Use dotenv file
 dotenv.config({ path: "./Configs/.env" });
@@ -32,9 +33,36 @@ const initAdmin = async () => {
    }
 };
 
+// Init admin
+const initEngineer = async () => {
+   const engineerEmail = process.env.ENGINEER_EMAIL;
+
+   try {
+      const existingEngineer = await EngineerModel.findOne({ email: engineerEmail });
+
+      if (existingEngineer) {
+         console.log("Engineer already exists.");
+         return;
+      }
+
+      const newEngineer = new EngineerModel({
+         firstName: process.env.ENGINEER_FIRST_NAME,
+         lastName: process.env.ENGINEER_LAST_NAME,
+         email: engineerEmail,
+         engineerStatus: process.env.ENGINEER_STATUS,
+         password: process.env.ENGINEER_PASSWORD
+      });
+
+      await newEngineer.save();
+      console.log("Engineer is created.");
+   } catch (error) {
+      console.error("Error creating engineer:", error);
+   }
+};
 const init = async () => {
    await connectDB();
    await initAdmin();
+   await initEngineer();
    app.listen(process.env.PORT, () => {
       console.log(`Server is up and running! Access it at http://localhost:${process.env.PORT}/api/v1`);
    });

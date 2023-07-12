fc4ee466bdb2db132a22e243f07a110738d3630a
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-07-12 19:49:46 +0530

    update server.js

--- server.js
+++ server.js
@@ -3,31 +3,41 @@ const app = require('./app');
 const { connectDB } = require('./Configs/config.DB');
 const AdminModel = require('./Models/AdminModel');
 
-
-// use dotenv file
+// Use dotenv file
 dotenv.config({ path: "./Configs/.env" });
 
-//init admin 
+// Init admin
 const initAdmin = async () => {
-   await AdminModel.findOneAndUpdate(
-      { email: process.env.admin_email },
-      {
+   const adminEmail = process.env.ADMIN_EMAIL;
+
+   try {
+      const existingAdmin = await AdminModel.findOne({ email: adminEmail });
+
+      if (existingAdmin) {
+         console.log("Admin already exists.");
+         return;
+      }
+
+      const newAdmin = new AdminModel({
          firstName: process.env.ADMIN_FIRST_NAME,
          lastName: process.env.ADMIN_LAST_NAME,
-         email: process.env.ADMIN_EMAIL,
+         email: adminEmail,
          password: process.env.ADMIN_PASSWORD
-      },
-      { upsert: true, new: true }
-   );
-   console.log("Admin is created.");
-};
+      });
 
+      await newAdmin.save();
+      console.log("Admin is created.");
+   } catch (error) {
+      console.error("Error creating admin:", error);
+   }
+};
 
 const init = async () => {
    await connectDB();
    await initAdmin();
    app.listen(process.env.PORT, () => {
       console.log(`Server is up and running! Access it at http://localhost:${process.env.PORT}/api/v1`);
-   })
-}
+   });
+};
+
 init();
